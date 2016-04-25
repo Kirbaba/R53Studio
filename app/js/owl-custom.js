017811a36d1627669406c55b9541d75dbda72aa7
Vladislav <v.kirbaba@gmail.com>
2016-04-25 15:00:58 +0300

    Карусель партнеры

--- app/js/owl-custom.js
+++ app/js/owl-custom.js
@@ -11,4 +11,17 @@ jQuery(document).ready(function($) {
             navigationText : false,
             pagination : false
       });
+
+      jQuery(".trust__carousel").owlCarousel({    
+            navigation : false, 
+            items : 6,
+            slideSpeed : 1000,
+            paginationSpeed : 400,
+            touchDrag: true,
+            autoplayHoverPause: true,
+            items: [6],
+            navigation: true,
+            navigationText : false,
+            pagination : false
+      });
 });
