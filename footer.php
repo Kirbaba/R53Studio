c664320de36ad530002d025ddb2316095a950646
Vladislav <v.kirbaba@gmail.com>
2016-05-03 17:01:46 +0300

    Акции + Футер

--- footer.php
+++ footer.php
@@ -9,6 +9,9 @@
 		<div class="container">
 			<!-- open .footer__nav -->
 			<nav class="footer__nav">
+				<!-- open .footer_title -->
+				<h3 class="footer_title">Меню</h3>
+				<!-- close .footer_title -->
 				<?php $args = array( // опции для вывода нижнего меню, чтобы они работали, меню должно быть создано в админке
 				'theme_location' => 'top', // идентификатор меню, определен в register_nav_menus() в function.php
 				'container'=> false, // обертка списка, false - это ничего
@@ -18,16 +21,38 @@
 			  	);
 				wp_nav_menu($args); // выводим нижние меню
 						?>
-				<!-- open .footer__copyright -->
-				<p class="footer__copyright">© 2015 R53Studio</p>
-				<!-- close .footer__copyright -->
+				
 			</nav>
 			<!-- close .footer__nav -->
+
+			<!-- open .footer__item -->
+			<div class="footer__item">
+				<!-- open .footer_title -->
+				<h3 class="footer_title">Контакты</h3>
+				<!-- close .footer_title -->
+				<p> +375 29 6 255 755</p>
+				<p>тел/факс: 8 02235 77277</p>
+				<p>r53-studio@mail.ru</p>
+			</div>
+			<!-- close .footer__item -->
+
+			<!-- open .footer__item -->
+			<div class="footer__item footer__item_soc">
+				<!-- open .footer_title -->
+				<h3 class="footer_title">Мы в соцсетях</h3>
+				<!-- close .footer_title -->
+				<a href="#" class="footer__item_soc_icon"><i class="fa fa-vk"></i></a>
+				<a href="#" class="footer__item_soc_icon"><i class="fa fa-facebook"></i></a>
+				<a href="#" class="footer__item_soc_icon"><i class="fa fa-google-plus"></i></a>
+			</div>
+			<!-- close .footer__item -->
 			<!-- open .footer__contacts -->
 			<div class="footer__contacts">
-				<h4>+375 <?= get_theme_mod('phone_1_textbox') ?></h4>
-				<p>Минск, улица, дом</p>
-			</div>
+			<!-- open .footer__copyright -->
+				<p class="footer__contacts_copyr">© 2015 R53Studio</p>
+				<!-- close .footer__copyright -->
+				<p class="footer__contacts_slogan">ОТЛИЧНОЕ КАЧЕСТВО ЗА РАЗУМНЫЕ ДЕНЬГИ</p>
+			</div>		
 			<!-- close .footer__contacts -->
 			
 		</div>
